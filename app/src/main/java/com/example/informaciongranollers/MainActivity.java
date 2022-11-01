5854394cdbc4fc27ee94a59db337bdac4498f9a3
geniscarmona094 <genis.carmonat@educem.net>
2022-11-01 22:18:14 +0100

    Orientation only portrait

--- app/src/main/java/com/example/informaciongranollers/MainActivity.java
+++ app/src/main/java/com/example/informaciongranollers/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.informaciongranollers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tienda = findViewById(R.id.btBussines);
+        tienda.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Button b = (Button)view;
+
+        if (b.getId() == R.id.btBussines ) {
+            //Obrim l'activitat 2 amb el Intent
+
+            Intent intent = new Intent(this, Businesses.class);  //.class és l'arxiu java compilat
+            startActivity(intent);
+        }
     }
 }
