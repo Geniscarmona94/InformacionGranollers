d9211482d5c9f8e690c84a46d6da3ed6aafe1ccd
geniscarmona094 <genis.carmonat@educem.net>
2022-11-18 10:08:33 +0100

    Update

--- app/src/main/java/com/example/informaciongranollers/fragments/AutoescuelaFragment.java
+++ app/src/main/java/com/example/informaciongranollers/fragments/AutoescuelaFragment.java
@@ -1,17 +1,39 @@
 package com.example.informaciongranollers.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.informaciongranollers.R;
 
-public class AutoescuelaFragment extends Fragment {
+public class AutoescuelaFragment extends Fragment implements View.OnClickListener {
 
+    TextView telefonoLiceu;
+
+    public AutoescuelaFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AutoescuelaFragment newInstance() {
+        AutoescuelaFragment fragment = new AutoescuelaFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,4 +41,23 @@ public class AutoescuelaFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_autoescuela, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        telefonoLiceu = view.findViewById(R.id.liceuTelef);
+        telefonoLiceu.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.liceuTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 678 63 15 64"));
+            startActivity(telfIntent);
+        }
+    }
+
 }
