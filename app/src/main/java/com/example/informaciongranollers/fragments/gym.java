2fe0e457ccc4ba1e7d48dbeca60635629e20badb
geniscarmona094 <genis.carmonat@educem.net>
2022-11-22 21:45:22 +0100

    Bussiness acabado

--- app/src/main/java/com/example/informaciongranollers/fragments/gym.java
+++ app/src/main/java/com/example/informaciongranollers/fragments/gym.java
@@ -1,21 +1,117 @@
 package com.example.informaciongranollers.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.informaciongranollers.R;
 
-public class gym extends Fragment {
+public class gym extends Fragment implements View.OnClickListener{
+
+    //CNG
+    ImageView CNGWeb;
+    ImageView CNGTelef;
+    ImageView CNGMap;
+
+    //anytimefitness
+    ImageView anytimefitnessWeb;
+    ImageView anytimefitnessTelef;
+    ImageView anytimefitnessMap;
+
+    //palacefitness
+    ImageView palacefitnessWeb;
+    ImageView palacefitnessTelef;
+    ImageView palacefitnessMap;
+
+    public gym(){
+        // Required empty public constructor
+    }
+
+    public static gym newInstance() {
+        gym fragment = new gym();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gym, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //CNG
+        CNGWeb = view.findViewById(R.id.CNGWeb);
+        CNGWeb.setOnClickListener(this);
+        CNGTelef = view.findViewById(R.id.CNGTelef);
+        CNGTelef.setOnClickListener(this);
+        CNGMap = view.findViewById(R.id.CNGMap);
+        CNGMap.setOnClickListener(this);
+
+        //anytimefitness
+        anytimefitnessTelef = view.findViewById(R.id.anytimefitnessTelef);
+        anytimefitnessTelef.setOnClickListener(this);
+        anytimefitnessWeb = view.findViewById(R.id.anytimefitnessWeb);
+        anytimefitnessWeb.setOnClickListener(this);
+        anytimefitnessMap = view.findViewById(R.id.anytimefitnessMap);
+        anytimefitnessMap.setOnClickListener(this);
+
+        //palacefitness
+        palacefitnessTelef = view.findViewById(R.id.palacefitnessTelef);
+        palacefitnessTelef.setOnClickListener(this);
+        palacefitnessWeb = view.findViewById(R.id.palacefitnessWeb);
+        palacefitnessWeb.setOnClickListener(this);
+        palacefitnessMap = view.findViewById(R.id.palacefitnessMap);
+        palacefitnessMap.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.CNGWeb){
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cngranollers.org/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.CNGTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 938 70 45 99"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.CNGMap) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.anytimefitnessWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.anytimefitness.es/gimnasio/sp-0014/gimnasio-granollers-catalu%C3%B1a-granollers/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.anytimefitnessTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 620 80 12 18"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.anytimefitnessMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.palacefitnessWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://palacefitness.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.palacefitnessTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 679 61 22 94"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.palacefitnessMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }
     }
 }
--- app/src/main/java/com/example/informaciongranollers/fragments/peluqueria.java
+++ app/src/main/java/com/example/informaciongranollers/fragments/peluqueria.java
@@ -1,21 +1,117 @@
 package com.example.informaciongranollers.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.informaciongranollers.R;
 
-public class peluqueria extends Fragment {
+public class peluqueria extends Fragment implements View.OnClickListener{
+
+    //aduho
+    ImageView aduhoWeb;
+    ImageView aduhoTelef;
+    ImageView aduhoMap;
+
+    //elsalon
+    ImageView elsalonWeb;
+    ImageView elsalonTelef;
+    ImageView elsalonMap;
+
+    //urbanbeauty
+    ImageView urbanbeautyWeb;
+    ImageView urbanbeautyTelef;
+    ImageView urbanbeautyMap;
+
+    public peluqueria() {
+        // Required empty public constructor
+    }
+
+    public static peluqueria newInstance() {
+        peluqueria fragment = new peluqueria();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_peluqueria, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //aduho
+        aduhoWeb = view.findViewById(R.id.aduhoWeb);
+        aduhoWeb.setOnClickListener(this);
+        aduhoTelef = view.findViewById(R.id.aduhoTelef);
+        aduhoTelef.setOnClickListener(this);
+        aduhoMap = view.findViewById(R.id.aduhoMap);
+        aduhoMap.setOnClickListener(this);
+
+        //elsalon
+        elsalonTelef = view.findViewById(R.id.elsalonTelef);
+        elsalonTelef.setOnClickListener(this);
+        elsalonWeb = view.findViewById(R.id.elsalonWeb);
+        elsalonWeb.setOnClickListener(this);
+        elsalonMap = view.findViewById(R.id.elsalonMap);
+        elsalonMap.setOnClickListener(this);
+
+        //urbanbeauty
+        urbanbeautyTelef = view.findViewById(R.id.urbanbeautyTelef);
+        urbanbeautyTelef.setOnClickListener(this);
+        urbanbeautyWeb = view.findViewById(R.id.urbanbeautyWeb);
+        urbanbeautyWeb.setOnClickListener(this);
+        urbanbeautyMap = view.findViewById(R.id.urbanbeautyMap);
+        urbanbeautyMap.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.aduhoWeb){
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://aduho.com/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.aduhoTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 938 70 14 87"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.aduhoMap) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.elsalonWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://elsalon.netlify.app/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.elsalonTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 938 44 33 39"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.elsalonMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.urbanbeautyWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.urbanbeautygranollers.com/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.urbanbeautyTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 938 79 31 75"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.urbanbeautyMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }
     }
 }
