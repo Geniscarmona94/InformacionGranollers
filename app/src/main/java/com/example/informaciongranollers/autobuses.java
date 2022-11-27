4862b0c8e1a2d933906a32d76ebebb1a7cce8928
geniscarmona094 <genis.carmonat@educem.net>
2022-11-27 23:11:08 +0100

    app finalizada

--- app/src/main/java/com/example/informaciongranollers/autobuses.java
+++ app/src/main/java/com/example/informaciongranollers/autobuses.java
@@ -2,13 +2,63 @@ package com.example.informaciongranollers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class autobuses extends AppCompatActivity {
+public class autobuses extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autobuses);
+
+        //transgran
+        ImageView transgran = findViewById(R.id.transgranImagen);
+        transgran.setOnClickListener(this);
+
+        //express
+        ImageView express = findViewById(R.id.expressImagen);
+        express.setOnClickListener(this);
+
+        //urbanas
+        ImageView urbanas = findViewById(R.id.urbanasImagen);
+        urbanas.setOnClickListener(this);
+
+        //interurbanas
+        ImageView interurbanas = findViewById(R.id.interurbanasImagen);
+        interurbanas.setOnClickListener(this);
+
+        //nocturnes
+        ImageView nocturnes = findViewById(R.id.nocturnesImagen);
+        nocturnes.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        ImageView b = (ImageView) view;
+
+        if (b.getId() == R.id.transgranImagen){
+            Intent busesIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sagales.com/uploads/imagenes/marcaliniesWebUrbans/marca_2.pdf"));
+            startActivity(busesIntent);
+        } else if(b.getId() == R.id.expressImagen)
+        {
+            Intent busesIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sagales.com/linies/4/linies-expres"));
+            startActivity(busesIntent);
+        }else if(b.getId() == R.id.urbanasImagen)
+        {
+            Intent busesIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sagales.com/linies/1/linies-urbanes"));
+            startActivity(busesIntent);
+        }else if(b.getId() == R.id.interurbanasImagen)
+        {
+            Intent busesIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sagales.com/linies/2/linies-interurbanes"));
+            startActivity(busesIntent);
+        }else if(b.getId() == R.id.nocturnesImagen)
+        {
+            Intent busesIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sagales.com/linies/3/linies-nocturnes"));
+            startActivity(busesIntent);
+        }
     }
 }
--- app/src/main/java/com/example/informaciongranollers/trenes.java
+++ app/src/main/java/com/example/informaciongranollers/trenes.java
@@ -2,8 +2,12 @@ package com.example.informaciongranollers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class trenes extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,10 +15,47 @@ public class trenes extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trenes);
+
+        //granollers centre
+        ImageView r2 = findViewById(R.id.r2);
+        r2.setOnClickListener(this);
+        ImageView r8 = findViewById(R.id.r8);
+        r8.setOnClickListener(this);
+        ImageView r11 = findViewById(R.id.r11);
+        r11.setOnClickListener(this);
+
+        //granollers canovelles
+        ImageView r3 = findViewById(R.id.r3);
+        r3.setOnClickListener(this);
+
+        //granollers nord (les franqueses)
+        ImageView r2Nord = findViewById(R.id.r2Nord);
+        r2Nord.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        ImageView b = (ImageView) view;
 
+        if (b.getId() == R.id.r2 ){
+            Intent trenIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R2.pdf"));
+            startActivity(trenIntent);
+        } else if(b.getId() == R.id.r8)
+        {
+            Intent trenIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/Grafica_CARTEL-HORARIOS-R8-desde-20-agosto-2022_PAT-Castellbisbal-Corredor-Mediterraneo.pdf"));
+            startActivity(trenIntent);
+        } else if(b.getId() == R.id.r11)
+        {
+            Intent trenIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R11.pdf"));
+            startActivity(trenIntent);
+        } else if(b.getId() == R.id.r3)
+        {
+            Intent trenIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R3.pdf"));
+            startActivity(trenIntent);
+        } else if(b.getId() == R.id.r2Nord)
+        {
+            Intent trenIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R2-NORD_R11_PAT-SAGRERA_Web.pdf"));
+            startActivity(trenIntent);
+        }
     }
 }
