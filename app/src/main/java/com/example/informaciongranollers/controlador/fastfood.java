0320303ad37bf857bc759f272c1f1fa9b15b1e50
joansv13 <joan.salavila03@gmail.com>
2022-11-27 22:21:58 +0100

    suvbidfa

--- app/src/main/java/com/example/informaciongranollers/controlador/fastfood.java
+++ app/src/main/java/com/example/informaciongranollers/controlador/fastfood.java
@@ -1,12 +1,17 @@
 package com.example.informaciongranollers.controlador;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.informaciongranollers.R;
 
@@ -15,7 +20,21 @@ import com.example.informaciongranollers.R;
  * Use the {@link fastfood#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fastfood extends Fragment {
+public class fastfood extends Fragment implements View.OnClickListener{
+    //mcdonals
+    ImageView mcdonalsWeb;
+    ImageView mcdonalsTelef;
+    ImageView mcdonalsMap;
+
+    //burger
+    ImageView burgerWeb;
+    ImageView burgerTelef;
+    ImageView burgerMap;
+
+    //kfc
+    ImageView kfcWeb;
+    ImageView kfcTelef;
+    ImageView kfcMap;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -51,10 +70,7 @@ public class fastfood extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -62,5 +78,70 @@ public class fastfood extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fastfood, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //mcdonals
+        mcdonalsTelef = view.findViewById(R.id.mcdonalsTelef);
+        mcdonalsTelef.setOnClickListener(this);
+        mcdonalsWeb = view.findViewById(R.id.mcdonalsWeb);
+        mcdonalsWeb.setOnClickListener(this);
+        mcdonalsMap = view.findViewById(R.id.mcdonalsMap);
+        mcdonalsMap.setOnClickListener(this);
+
+        //burger
+        burgerTelef = view.findViewById(R.id.burgerTelef);
+        burgerTelef.setOnClickListener(this);
+        burgerWeb = view.findViewById(R.id.burgerWeb);
+        burgerWeb.setOnClickListener(this);
+        burgerMap = view.findViewById(R.id.burgerMap);
+        burgerMap.setOnClickListener(this);
+
+        //kfc
+        kfcTelef = view.findViewById(R.id.kfcTelef);
+        kfcTelef.setOnClickListener(this);
+        kfcWeb = view.findViewById(R.id.kfcWeb);
+        kfcWeb.setOnClickListener(this);
+        kfcMap = view.findViewById(R.id.kfcMap);
+        kfcMap.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.kfcWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfc.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.kfcTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 938 79 51 07"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.kfcMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.burgerWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.burgerking.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.burgerTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 676 64 51 26"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.burgerMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.mcdonalsWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://mcdonalds.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.mcdonalsTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 615 73 65 34"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.mcdonalsMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }
     }
 }
--- app/src/main/java/com/example/informaciongranollers/parking.java
+++ app/src/main/java/com/example/informaciongranollers/parking.java
@@ -10,17 +10,79 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class parking extends AppCompatActivity implements View.OnClickListener {
+    //atenea
+    ImageView ateneaWeb;
+    ImageView ateneaTelef;
+    ImageView ateneaMap;
 
+    //elsot
+    ImageView elsotWeb;
+    ImageView elsotTelef;
+    ImageView elsotMap;
+
+    //corona
+    ImageView coronaWeb;
+    ImageView coronaTelef;
+    ImageView coronaMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
 
+        //elsot
+        elsotTelef = findViewById(R.id.elsotTelef);
+       elsotTelef.setOnClickListener(this);
+        elsotWeb = findViewById(R.id.elsotWeb);
+        elsotWeb.setOnClickListener(this);
+        elsotMap = findViewById(R.id.elsotMap);
+        elsotMap.setOnClickListener(this);
 
+        //atenea
+        ateneaTelef = findViewById(R.id.ateneaTelef);
+        ateneaTelef.setOnClickListener(this);
+        ateneaWeb = findViewById(R.id.ateneaWeb);
+        ateneaWeb.setOnClickListener(this);
+        ateneaMap = findViewById(R.id.ateneaMap);
+        ateneaMap.setOnClickListener(this);
+
+        //corona
+        coronaTelef = findViewById(R.id.coronaTelef);
+        coronaTelef.setOnClickListener(this);
+        coronaWeb = findViewById(R.id.coronaWeb);
+        coronaWeb.setOnClickListener(this);
+        coronaMap = findViewById(R.id.coronaMap);
+        coronaMap.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view.getId() == R.id.coronaWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.coronaTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 938 606 690"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.coronaMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.elsotWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.elsotTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 938 709 150"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.elsotMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }else if (view.getId() == R.id.ateneaWeb) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.es/"));
+            startActivity(webIntent);
+        }else if (view.getId() == R.id.ateneaTelef) {
+            Intent telfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel: 618 463 836"));
+            startActivity(telfIntent);
+        }else if (view.getId() == R.id.ateneaMap){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: ")); //no se como poner la ubicación en latitud y longuitud
+            startActivity(mapIntent);
+        }
     }
 }
